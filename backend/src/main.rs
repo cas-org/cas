12d0a7b5e7ac165855354f1e3b5d93d7d6140f52
Woong Ze Yi <woongzeyi@gmail.com>
2021-09-18 17:42:36 +0800

    Prepare for multi-page React website

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,21 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::fs::{relative, FileServer, NamedFile};
+
+#[get("/<_..>", rank = 2)]
+async fn not_found_rewrite() -> Option<NamedFile> {
+    NamedFile::open(relative!("../frontend/build/index.html"))
+        .await
+        .ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount(
+            "/",
+            FileServer::from(relative!("../frontend/build")).rank(1),
+        )
+        .mount("/", routes![not_found_rewrite])
 }
