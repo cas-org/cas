851105a5e7051f0befd393e478f84e0800f81e99
Woong Ze Yi <woongzeyi@gmail.com>
2021-10-29 21:01:26 +0800

    Replace x.py with taskfile

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,18 +5,16 @@ use std::env;
 
 use rocket::fs::{relative, FileServer, NamedFile};
 
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", FileServer::from(relative!("../frontend/build")).rank(1))
+        .mount("/", routes![not_found_rewrite])
+}
+
 #[get("/<_..>", rank = 2)]
 async fn not_found_rewrite() -> Option<NamedFile> {
     NamedFile::open(relative!("../frontend/build/index.html"))
         .await
         .ok()
-}
-
-#[launch]
-fn rocket() -> _ {
-    let mut path = env::current_dir().unwrap();
-    path.push("page");
-    rocket::build()
-        .mount("/", FileServer::from(path).rank(1))
-        .mount("/", routes![not_found_rewrite])
 }
