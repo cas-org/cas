a140bfee6cc3c14c49fe99af56b6fa5e1d204be4
Woong Ze Yi <woongzeyi@gmail.com>
2021-11-04 15:25:10 +0800

    Implement Single Page Application serving in backend

--- backend/backend/__main__.py
+++ backend/backend/__main__.py
@@ -1,10 +1,38 @@
-from typing import Optional
+from typing import Optional, Tuple
+import os
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+class SinglePageApplication(StaticFiles):
+    """Acts similar to the bripkens/connect-history-api-fallback
+    NPM package."""
+
+    def __init__(self, directory: os.PathLike, index='index.html') -> None:
+        self.index = index
+
+        # set html=True to resolve the index even when no
+        # the base path is passed in
+        super().__init__(directory=directory, packages=None, html=True, check_dir=True)
+
+    async def lookup_path(self, path: str) -> Tuple[str, os.stat_result]:
+        """Returns the index file when no match is found.
+
+        Args:
+            path (str): Resource path.
+
+        Returns:
+            [tuple[str, os.stat_result]]: Always retuens a full path and stat result.
+        """
+        full_path, stat_result = await super().lookup_path(path)
+
+        # if a file cannot be found
+        if stat_result is None:
+            return await super().lookup_path(self.index)
+
+        return (full_path, stat_result)
 
 app = FastAPI()
+app.mount("/", SinglePageApplication(directory="../frontend/build"), name="app")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
