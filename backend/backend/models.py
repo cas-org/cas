8e5189a2c55e6d71e356b9a8aa6d8be44382def2
jamesfte <ftejt1408@gmail.com>
2021-11-21 00:25:52 +0800

    Implement QuestionContent

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -36,11 +36,38 @@ class OccurringType(Enum):
     CONTEST = auto()
 
 
+class QuestionType(Enum):
+    Objective = auto()
+    Subjective = auto()
+    TrueOrFalse = auto()
+    MultipleOption = auto()
+    
+class OptionStatus(Enum):
+    Option = auto()
+    Answer = auto()
+    
+class QuestionOption:
+    status: OptionStatus
+    option1: str
+    option2: str
+    option3: str
+    option4: str
+
+class QuestionStatement:
+    Statementid: ObjectId  
+    Statement: str  
+
+class QuestionContent:
+    ref: ObjectId
+    status: QuestionType
+    statement: QuestionStatement
+    option: Optional[List[QuestionOption]]
+    
+
 class Occurring(BaseModel):
     display: str
     type: OccurringType
-    # TODO Decide what occuring content should consist of
-    # content: OccurringContent
+    content: List[QuestionContent]
     created_on: Timestamp
     edited_on: Timestamp
 
